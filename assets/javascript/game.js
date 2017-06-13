46e919b7399bf0bb0602ba60fa8f62f767dff4d1
MB DR <mderoberts@users.noreply.github.com>
2017-06-12 23:12:11 -0400

    random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-    var wins = 0;
-    var losses = 0;
-    var randomNum1 = function(){
-        Math.floor((Math.random() * 120) +19);
+    var scoreboard = { 
+        wins: 0,
+        losses: 0
     };
-    var randomNum2 = function(){
-        Math.floor((Math.random() * 12) +1);
-    };
+
+    function randomNum(x, y) {
+         return Math.floor(Math.random() * (y - x + 1)) + x;
+    }
+
+    $("#gem-1").attr("value", randomNum(1,12));
+    $("#gem-2").attr("value", randomNum(1,12));
+    $("#gem-3").attr("value", randomNum(1,12));
+    $("#gem-4").attr("value", randomNum(1,12));
+    // console.log($("#gem-1").attr("value"));
+
+    $("#matchNum").text(randomNum(19, 120));
+
+
+
 });
