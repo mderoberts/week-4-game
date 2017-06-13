2e3a5be7dd7b5cd35b44e7463369e19a214b59da
MB DR <mderoberts@users.noreply.github.com>
2017-06-13 00:41:12 -0400

    number match, score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,25 @@ $(document).ready(function(){
         losses: 0
     };
 
+
     function randomNum(x, y) {
-         return Math.floor(Math.random() * (y - x + 1)) + x;
+        return Math.floor(Math.random() * (y - x + 1)) + x;
     }
 
     $("#gem-1").attr("value", randomNum(1,12));
     $("#gem-2").attr("value", randomNum(1,12));
     $("#gem-3").attr("value", randomNum(1,12));
     $("#gem-4").attr("value", randomNum(1,12));
+
+    var score = 0;
+    $(".gem").click(function(){
+        score = Number(randomNum(1,12)) + Number($(this).val());
+            $("#score").text(score);
+    });
+
     // console.log($("#gem-1").attr("value"));
 
     $("#matchNum").text(randomNum(19, 120));
-
 
 
 });
