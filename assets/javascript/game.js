cd6c6142965677be0d0b98a1f9fc9457194ec334
MB DR <mderoberts@users.noreply.github.com>
2017-06-12 21:47:57 -0400

    add JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var wins = 0;
+    var losses = 0;
+    var randomNum1 = function(){
+        Math.floor((Math.random() * 120) +19);
+    };
+    var randomNum2 = function(){
+        Math.floor((Math.random() * 12) +1);
+    };
+});
