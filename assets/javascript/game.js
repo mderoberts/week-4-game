c12efd6ec0675d462f28ff1882f322f39f5894b5
MB DR <mderoberts@users.noreply.github.com>
2017-06-17 09:17:56 -0400

    bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,20 +9,60 @@ $(document).ready(function(){
         return Math.floor(Math.random() * (y - x + 1)) + x;
     }
 
-    $("#gem-1").attr("value", randomNum(1,12));
-    $("#gem-2").attr("value", randomNum(1,12));
-    $("#gem-3").attr("value", randomNum(1,12));
-    $("#gem-4").attr("value", randomNum(1,12));
+    function resetGame() {
+
+    $("#gem-1").data("randomNum", randomNum(1,12));
+    $("#gem-2").data("randomNum", randomNum(1,12));
+    $("#gem-3").data("randomNum", randomNum(1,12));
+    $("#gem-4").data("randomNum", randomNum(1,12));
+    console.log($("#gem-1").data("randomNum"));
+    console.log($("#gem-2").data("randomNum"));
+    console.log($("#gem-3").data("randomNum"));
+    console.log($("#gem-4").data("randomNum"));
+
+    matchNum = randomNum(19, 120);
+    $("#matchNum").html(matchNum);
+
+    score = 0;
+    $("#score").html(score);
+
+
+}
+
+    $("#gem-1").data("randomNum", randomNum(1,12));
+    $("#gem-2").data("randomNum", randomNum(1,12));
+    $("#gem-3").data("randomNum", randomNum(1,12));
+    $("#gem-4").data("randomNum", randomNum(1,12));
+    console.log($("#gem-1").data("randomNum"));
+    console.log($("#gem-2").data("randomNum"));
+    console.log($("#gem-3").data("randomNum"));
+    console.log($("#gem-4").data("randomNum"));
+
+    var matchNum = randomNum(19, 120);
+    $("#matchNum").html(matchNum);
+
 
     var score = 0;
     $(".gem").click(function(){
-        score = Number(randomNum(1,12)) + Number($(this).val());
-            $("#score").text(score);
+        var gemValue = parseInt($(this).data("randomNum"));
+        score = score + gemValue;
+            $("#score").html(score);
+            console.log("score: " + score);
+        if (score === matchNum) {
+            scoreboard.wins++;
+            $("#wins").html("Wins: " + scoreboard.wins);
+            alert("You win!");
+            resetGame();
+        }
+
+        else if (score > matchNum) {
+            scoreboard.losses++;
+            $("#losses").html("Losses: " + scoreboard.losses);
+            alert("You lose!");
+            resetGame();
+        }
+
     });
-
-    // console.log($("#gem-1").attr("value"));
-
-    $("#matchNum").text(randomNum(19, 120));
 
 
 });
